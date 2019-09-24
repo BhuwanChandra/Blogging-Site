6755b5b7575ca7fa3e1bbc8411b40482b4c4391b
BhuwanChandra <bhuwan7779@gmail.com>
2019-09-24 22:23:03 +0530

    created Blog schema

---
+++ models/blogs.js
@@ -0,0 +1,29 @@
+var mongoose = require("mongoose");
+
+
+// Schema Setup
+var blogSchema = new mongoose.Schema({
+    name: String,
+    image: String,
+    body: String,
+    created: {
+        type: Date,
+        default: Date.now
+    },
+    author: {
+        id: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User"
+        },
+        username: String
+    },
+    comments: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Comment"
+        }
+    ]
+});
+
+module.exports = mongoose.model("Blog", blogSchema);
+
